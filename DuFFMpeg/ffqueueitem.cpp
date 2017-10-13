2fb0df3e297a3a48c849a196ab37f6ef4e76e0a7
Duduf <git@duduf.com>
2017-10-13 18:58:14 +0200

    queue item implementation

--- DuFFMpeg/ffqueueitem.cpp
+++ DuFFMpeg/ffqueueitem.cpp
@@ -1,6 +1,87 @@
 #include "ffqueueitem.h"
 
-FFQueueItem::FFQueueItem()
+FFQueueItem::FFQueueItem(QList<FFMediaInfo *> inputs, QList<FFMediaInfo *> outputs, QObject *parent)
 {
+    inputMedias = inputs;
+    outputMedias = outputs;
+    status = Waiting;
+}
 
+FFQueueItem::FFQueueItem(FFMediaInfo *input, QList<FFMediaInfo *> outputs)
+{
+    inputMedias << input;
+    outputMedias = outputs;
+    status = Waiting;
+}
+
+FFQueueItem::FFQueueItem(FFMediaInfo *input, FFMediaInfo *output)
+{
+    inputMedias << input;
+    outputMedias << output;
+    status = Waiting;
+}
+
+QList<FFMediaInfo *> FFQueueItem::getInputMedias()
+{
+    return inputMedias;
+}
+
+QList<FFMediaInfo *> FFQueueItem::getOutputMedias()
+{
+    return outputMedias;
+}
+
+int FFQueueItem::addInputMedia(FFMediaInfo *input)
+{
+    inputMedias << input;
+    return inputMedias.count() - 1;
+}
+
+int FFQueueItem::addOutputMedia(FFMediaInfo *output)
+{
+    outputMedias << output;
+    return outputMedias.count() -1;
+}
+
+FFMediaInfo *FFQueueItem::removeInputMedia(int id)
+{
+    return inputMedias.takeAt(id);
+}
+
+FFMediaInfo *FFQueueItem::removeInputMedia(QString fileName)
+{
+    for (int i = 0 ; i < inputMedias.count() ; i++)
+    {
+        if (inputMedias[i]->getFileName() == fileName)
+        {
+            return inputMedias.takeAt(i);
+        }
+    }
+}
+
+FFMediaInfo *FFQueueItem::removeOutputMedia(int id)
+{
+    return outputMedias.takeAt(id);
+}
+
+FFMediaInfo *FFQueueItem::removeOutputMedia(QString fileName)
+{
+    for (int i = 0 ; i < outputMedias.count() ; i++)
+    {
+        if (outputMedias[i]->getFileName() == fileName)
+        {
+            return outputMedias.takeAt(i);
+        }
+    }
+}
+
+FFQueueItem::Status FFQueueItem::getStatus()
+{
+    return status;
+}
+
+void FFQueueItem::setStatus(Status st)
+{
+    status = st;
+    emit statusChanged(status);
 }
--- DuFFMpeg/ffqueueitem.h
+++ DuFFMpeg/ffqueueitem.h
@@ -1,11 +1,51 @@
 #ifndef FFQUEUEITEM_H
 #define FFQUEUEITEM_H
 
+#include <QObject>
 
-class FFQueueItem
+#include "ffmediainfo.h"
+
+class FFQueueItem : public QObject
 {
+        Q_OBJECT
 public:
-    FFQueueItem();
+    FFQueueItem(QList<FFMediaInfo*> inputs,QList<FFMediaInfo*> outputs,QObject *parent = nullptr);
+    FFQueueItem(FFMediaInfo *input,QList<FFMediaInfo*> outputs);
+    FFQueueItem(FFMediaInfo *input,FFMediaInfo *output);
+
+    /**
+     * @brief The Status enum Used to describe the current status of the item
+     */
+    enum Status { Waiting, InProgress, Finished, Stopped };
+    Q_ENUM(Status)
+
+    QList<FFMediaInfo*> getInputMedias();
+    QList<FFMediaInfo*> getOutputMedias();
+    int addInputMedia(FFMediaInfo *input);
+    int addOutputMedia(FFMediaInfo *output);
+    FFMediaInfo *removeInputMedia(int id);
+    FFMediaInfo *removeInputMedia(QString fileName);
+    FFMediaInfo *removeOutputMedia(int id);
+    FFMediaInfo *removeOutputMedia(QString fileName);
+    Status getStatus();
+
+public slots:
+    /**
+     * @brief setStatus Changes the status of the item
+     * @param st The status
+     */
+    void setStatus(Status st);
+
+signals:
+    void encodingStarted();
+    void encodingStopped();
+    void encodingFinished();
+    void statusChanged(Status);
+
+private:
+    QList<FFMediaInfo*> inputMedias;
+    QList<FFMediaInfo*> outputMedias;
+    Status status;
 };
 
 #endif // FFQUEUEITEM_H
