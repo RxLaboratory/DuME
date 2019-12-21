e837a7f03c337146b3f8e1256f99eb57ae8be0fd
Duduf <git@duduf.com>
2019-12-21 21:10:18 +0100

    improved image_copy test

--- DuFFMpeg/duexr.cpp
+++ DuFFMpeg/duexr.cpp
@@ -7,7 +7,7 @@ using namespace OIIO;
 DuEXR::DuEXR(QObject *parent) : QObject(parent)
 {
     //test
-
+/*
     auto in = ImageInput::open ("/home/duduf/DEV_SRC/DuFFMpeg/Test Medias/EXR (from Ae)/MYT_0046.003_00000.exr");
     if (! in)
         return;
@@ -20,19 +20,21 @@ DuEXR::DuEXR(QObject *parent) : QObject(parent)
     int xres = spec.width;
     int yres = spec.height;
     int channels = spec.nchannels;
-    float *pixels = new float [xres * yres * channels];;
+    float *pixels = new float [xres * yres * channels];
     in->read_image (TypeDesc::FLOAT, pixels);
     in->close ();
 
-    const char *filename = "/home/duduf/DEV_SRC/DuFFMpeg/Test Medias/EXR (from Ae)/MYT_0046.003_00000.jpg";
+    const char *filename = "/home/duduf/DEV_SRC/DuFFMpeg/Test Medias/EXR (from Ae)/MYT_0046.003_00000b.exr";
 
     std::unique_ptr<ImageOutput> out = ImageOutput::create (filename);
     if (! out)
         return;
     ImageSpec outSpec (xres, yres, channels, TypeDesc::FLOAT);
-    //out->open (filename, spec);
-    //out->write_image (TypeDesc::FLOAT, pixels);
-    //out->close ();
+    outSpec.attribute("compression","none");
+    out->open (filename, spec);
+    out->write_image (TypeDesc::FLOAT, pixels);
+    out->close ();
+*/
 
     //test copy image
     // Open the input file
@@ -41,13 +43,26 @@ DuEXR::DuEXR(QObject *parent) : QObject(parent)
 
     // Make an output spec, identical to the input except for metadata
     ImageSpec out_spec = cin->spec();
-    //out_spec.attribute("compression","none");
+
+#ifdef QT_DEBUG
+    qDebug() << "input compression";
+    qDebug() << QString::fromStdString( out_spec.decode_compression_metadata().first.str() );
+    qDebug() << out_spec.decode_compression_metadata().second;
+#endif
+
+    out_spec.attribute("compression","none");
+
+#ifdef QT_DEBUG
+    qDebug() << "output compression";
+    qDebug() << QString::fromStdString( out_spec.decode_compression_metadata().first.str() );
+    qDebug() << out_spec.decode_compression_metadata().second;
+#endif
 
     // Create the output file and copy the image
     const char *output = "/home/duduf/DEV_SRC/DuFFMpeg/Test Medias/EXR (from Ae)/MYT_0046.003_00001b.exr";
     std::unique_ptr<ImageOutput> cout = ImageOutput::create (output);
-    out->open (output, out_spec);
-    out->copy_image (cin.get());
+    cout->open (output, out_spec);
+    cout->copy_image (cin.get());
 
     // Clean up
     cout->close ();
