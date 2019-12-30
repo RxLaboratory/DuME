e048eece158b6bf4407371aefbff95f2a7d13c62
Duduf <git@duduf.com>
2019-12-30 19:07:56 +0100

    fix #124

--- src/UI/Blocks/blockframerate.cpp
+++ src/UI/Blocks/blockframerate.cpp
@@ -4,6 +4,19 @@ BlockFrameRate::BlockFrameRate(MediaInfo *mediaInfo, QWidget *parent) :
     UIBlockContent(mediaInfo, parent)
 {
     setupUi(this);
+
+    _presets->addAction( action8_fps );
+    _presets->addAction( action12_fps );
+    _presets->addAction( action15_fps );
+    _presets->addAction( action23_976_fps );
+    _presets->addAction( action24_fps );
+    _presets->addAction( action25_fps );
+    _presets->addAction( action29_97_fps );
+    _presets->addAction( action30_fps );
+    _presets->addAction( action50_fps );
+    _presets->addAction( action59_94_fps );
+    _presets->addAction( action60_fps );
+    _presets->addAction( action120_fps );
 }
 
 void BlockFrameRate::setActivated(bool activate)
@@ -24,7 +37,6 @@ void BlockFrameRate::update()
     if ( f != 0.0)
     {
         frameRateEdit->setValue( f );
-        frameRateBox->setCurrentText( QString::number( f ) + " fps" );
     }
 }
 
@@ -35,16 +47,67 @@ void BlockFrameRate::setFrameRate(double f)
     _freezeUI = false;
 }
 
-void BlockFrameRate::on_frameRateBox_activated(const QString &arg1)
-{
-    if ( _freezeUI ) return;
-    if (arg1 == "Custom") return;
-
-    QString num = frameRateBox->currentText().replace(" fps","");
-    _mediaInfo->setVideoFramerate( num.toDouble() );
-}
-
 void BlockFrameRate::on_frameRateEdit_valueChanged(double arg1)
 {
     _mediaInfo->setVideoFramerate( arg1 );
+}
+
+void BlockFrameRate::on_action8_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 8 );
+}
+
+void BlockFrameRate::on_action12_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 12 );
+}
+
+void BlockFrameRate::on_action15_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 15 );
+}
+
+void BlockFrameRate::on_action23_976_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 23.976 );
+}
+
+void BlockFrameRate::on_action24_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 24 );
+}
+
+void BlockFrameRate::on_action25_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 25 );
+}
+
+void BlockFrameRate::on_action29_97_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 29.97 );
+}
+
+void BlockFrameRate::on_action30_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 30 );
+}
+
+void BlockFrameRate::on_action50_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 50 );
+}
+
+void BlockFrameRate::on_action59_94_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 59.94 );
+}
+
+void BlockFrameRate::on_action60_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 60 );
+}
+
+void BlockFrameRate::on_action120_fps_triggered()
+{
+    _mediaInfo->setVideoFramerate( 120 );
 }
--- src/UI/Blocks/blockframerate.h
+++ src/UI/Blocks/blockframerate.h
@@ -16,8 +16,19 @@ public slots:
     void update();
     void setFrameRate( double f );
 private slots:
-    void on_frameRateBox_activated(const QString &arg1);
     void on_frameRateEdit_valueChanged(double arg1);
+    void on_action8_fps_triggered();
+    void on_action12_fps_triggered();
+    void on_action15_fps_triggered();
+    void on_action23_976_fps_triggered();
+    void on_action24_fps_triggered();
+    void on_action25_fps_triggered();
+    void on_action29_97_fps_triggered();
+    void on_action30_fps_triggered();
+    void on_action50_fps_triggered();
+    void on_action59_94_fps_triggered();
+    void on_action60_fps_triggered();
+    void on_action120_fps_triggered();
 };
 
 #endif // BLOCKFRAMERATE_H
