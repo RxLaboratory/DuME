c8bae42efe17f99ea34f5fe8b9414975182b608d
Duduf <git@duduf.com>
2020-01-09 20:21:17 +0100

    implementation of stream mapping

--- src/UI/Blocks/blockmapping.cpp
+++ src/UI/Blocks/blockmapping.cpp
@@ -1,18 +1,80 @@
 #include "blockmapping.h"
 #include "ui_blockmapping.h"
 
-BlockMapping::BlockMapping(MediaInfo *mediaInfo, QWidget *parent) :
-    UIBlockContent(mediaInfo,parent)
+BlockMapping::BlockMapping(MediaInfo *mediaInfo, MediaList *inputMedias, QWidget *parent) :
+    UIBlockContent(mediaInfo,inputMedias,parent)
 {
     setupUi(this);
 }
 
 void BlockMapping::setActivated(bool activate)
 {
-
+    _freezeUI = true;
+    if (activate)
+    {
+        foreach( StreamReferenceWidget *sw, _streamWidgets )
+        {
+            _mediaInfo->addMap( sw->mediaId(), sw->streamId() );
+        }
+    }
+    else
+    {
+        _mediaInfo->removeAllMaps();
+    }
+    _freezeUI = false;
 }
 
 void BlockMapping::update()
 {
+    _freezeUI = true;
 
+    qDeleteAll(_streamWidgets);
+    _streamWidgets.clear();
+
+    QList<StreamReference> maps = _mediaInfo->maps();
+
+    foreach( StreamReference map, maps)
+    {
+        addStreamWidget(map.mediaId(), map.streamId());
+    }
+
+    _freezeUI = false;
+}
+
+void BlockMapping::on_addButton_clicked()
+{
+    addStreamWidget();
+    _mediaInfo->addMap( _streamWidgets.last()->mediaId(), _streamWidgets.last()->streamId() );
+}
+
+void BlockMapping::changeStream(int index, int streamId)
+{
+    _mediaInfo->maps()[index].setStreamId(streamId);
+}
+
+void BlockMapping::changeMedia(int index, int mediaId)
+{
+    _mediaInfo->maps()[index].setMediaId(mediaId);
+}
+
+void BlockMapping::addStreamWidget(int mediaId, int streamId)
+{
+    StreamReferenceWidget *sw = new StreamReferenceWidget( _streamWidgets.count(), _inputMedias, this );
+    //TODO add medias
+    //TODO connect medias added/removed
+    sw->setMediaId( mediaId );
+    sw->setStreamId( streamId );
+    connect( sw, SIGNAL(removed(int)), this, SLOT (removeStreamWidget(int)));
+    connect( sw, SIGNAL(mediaChanged(int, int)), this, SLOT(changeMedia(int, int)));
+    connect( sw, SIGNAL(streamChanged(int, int)), this, SLOT(changeStream(int, int)));
+    _streamWidgets << sw;
+    mainLayout->addWidget(sw);
+}
+
+void BlockMapping::removeStreamWidget(int id)
+{
+    StreamReferenceWidget *sw = _streamWidgets.takeAt(id);
+    mainLayout->removeWidget(sw);
+    sw->deleteLater();
+    _mediaInfo->removeMap(id);
 }
--- src/UI/Blocks/blockmapping.h
+++ src/UI/Blocks/blockmapping.h
@@ -5,17 +5,28 @@
 
 #include "ui_blockmapping.h"
 #include "UI/Blocks/uiblockcontent.h"
+#include "UI/streamreferencewidget.h"
 
 class BlockMapping : public UIBlockContent, private Ui::BlockMapping
 {
     Q_OBJECT
 
 public:
-    explicit BlockMapping(MediaInfo *mediaInfo, QWidget *parent = nullptr);
+    explicit BlockMapping(MediaInfo *mediaInfo, MediaList *inputMedias, QWidget *parent = nullptr);
 
 public slots:
     void setActivated( bool activate );
     void update();
+
+private slots:
+    void on_addButton_clicked();
+    void changeStream(int index, int streamId);
+    void changeMedia(int index, int mediaId);
+    void removeStreamWidget(int id);
+
+private:
+    QList<StreamReferenceWidget *> _streamWidgets;
+    void addStreamWidget(int mediaId = -1, int streamId = -1);
 
 };
 
