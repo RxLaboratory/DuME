7ff211e996563e0c6337b3baf3fd1542bbc08b1c
Duduf <Duduf@DUDUF-DESKTOP.local>
2022-01-20 14:37:39 +0100

    updated version info

--- src/duqf-app/app-version.h
+++ src/duqf-app/app-version.h
@@ -2,9 +2,9 @@
 #define APPVERSION_H
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
+#define VERSION_MINOR 4
 #define VERSION_BUILD 0
-#define VERSION_SUFFIX ""
+#define VERSION_SUFFIX "Beta"
 
 #define STRINGIFY_VERSION(A, B, C) CONCAT(A, B, C )
 #define CONCAT(A, B, C ) STRINGIFY( A##.##B##.##C )
@@ -23,10 +23,10 @@
 #define VER_PRODUCTVERSION VERSION_MAJOR,VERSION_MINOR,VERSION_BUILD,0
 #define STR_PRODUCTVERSION STR_VERSION
 
-#define STR_COMPANYNAME "Rainbox Laboratory"
+#define STR_COMPANYNAME "RxLaboratory"
 #define STR_FILEDESCRIPTION "DuME"
 #define STR_INTERNALNAME "DuME"
-#define STR_LEGALCOPYRIGHT "Copyright (c) 2020-2021 Rainbox Laboratory, Nicolas Dufresne and contributors"
+#define STR_LEGALCOPYRIGHT "Copyright (c) 2020-2022 RxLaboratory, Nicolas Dufresne and contributors"
 #define STR_LEGALTRADEMARKS1 "All Rights Reserved"
 #define STR_ORIGINALFILENAME "DuME.exe"
 #define STR_PRODUCTNAME "DuME - Duduf Media Encoder"
@@ -34,10 +34,12 @@
 
 #define STR_COMPANYDOMAIN "rainboxlab.org"
 
-#define URL_CHAT "http://chat.rainboxlab.org"
-#define URL_FORUM "https://forum.rainboxlab.org"
-#define URL_DOC "https://dume-docs.rainboxlab.org"
+#define URL_CHAT "http://chat.rxlab.info"
+#define URL_FORUM ""
+#define URL_DOC "http://dume.rxlab.guide"
 #define URL_BUGREPORT "https://github.com/Rainbox-dev/DuME/issues/new?assignees=&labels=Bug&template=bug_report.md&title="
+#define URL_DONATION "http://donate.rxlab.info"
+#define URL_UPDATE "http://version.rxlab.io"
 
 #define APP_ICON ":/icons/app"
 #define SPLASH_IMAGE ":/images/splash"
